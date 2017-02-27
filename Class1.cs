0a8f6cbf30aa9125f9639325d47b157bfc78795e
tlogan <tlogan@hksinc.com>
2017-02-26 19:02:45 -0600

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.Revit;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace LINE.Revit
+{
+    [Transaction(TransactionMode.Manual)]
+    public class RandomizerCmd : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            try
+            {
+                RandomizerWindow window = new RandomizerWindow(commandData.Application.ActiveUIDocument.Document);
+                window.ShowDialog();
+                return Result.Succeeded;
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+                return Result.Failed;
+            }
+        }
+    }
+}
---
+++ RandomizerWindow.xaml.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using Autodesk.Revit;
+using Autodesk.Revit.DB;
+
+namespace LINE.Revit
+{
+    /// <summary>
+    /// Interaction logic for RandomizerWindow.xaml
+    /// </summary>
+    public partial class RandomizerWindow : Window
+    {
+        Document doc;
+        Dictionary<string, Element> typeElems;
+        FamilySymbol selectedSymbol = null;
+        string selectedParamName = null;
+
+        public RandomizerWindow(Document document)
+        {
+            doc = document;
+            
+            InitializeComponent();
+
+            Categories categories = doc.Settings.Categories;
+            List<Category> cats = new List<Category>();
+            foreach (Category cat in categories)
+            {
+                cats.Add(cat);
+            }
+            cats.Sort((x, y) => x.Name.CompareTo(y.Name));
+
+            categoryComboBox.ItemsSource = cats;
+            categoryComboBox.DisplayMemberPath = "Name";
+            categoryComboBox.SelectedIndex = 0;
+        }
+
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            try { DragMove(); }
+            catch { }
+        }
+
+        private void categoryComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Category cat = categoryComboBox.SelectedItem as Category;
+
+            if (cat != null)
+            {
+                List<string> elementNames = new List<string>();
+                FilteredElementCollector collector = new FilteredElementCollector(doc).OfCategoryId(cat.Id).WhereElementIsElementType();
+                typeElems = new Dictionary<string, Element>();
+                foreach (Element elem in collector)
+                {
+                    try
+                    {
+                        ElementType eType = elem as ElementType;
+                        FamilyInstanceFilter instFilter = new FamilyInstanceFilter(doc, eType.Id);
+                        ICollection<ElementId> instanceCol = new FilteredElementCollector(doc).OfCategoryId(eType.Category.Id).WherePasses(instFilter).ToElementIds();
+                        if (instanceCol.Count == 0)
+                            continue;
+
+                        string name = eType.FamilyName + ": " + eType.Name + " [" + instanceCol.Count.ToString() + "]";
+                        elementNames.Add(name);
+                        typeElems.Add(name, elem);
+                    }
+                    catch { }
+                }
+                elementNames.Sort();
+
+                typeComboBox.ItemsSource = elementNames;
+                typeComboBox.SelectedIndex = 0;
+            }
+            else
+                typeComboBox.ItemsSource = "Invalid Category";
+        }
+
+        private void typeComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string elementName = typeComboBox.SelectedItem as string;
+
+            Element selectedType = typeElems[elementName];
+            if(selectedType != null)
+            {
+                // Try to find an instance of this type.
+                ElementType eType = selectedType as ElementType;
+                FamilyInstanceFilter instFilter = new FamilyInstanceFilter(doc, eType.Id);
+                ICollection<ElementId> instanceCol = new FilteredElementCollector(doc).OfCategoryId(eType.Category.Id).WherePasses(instFilter).ToElementIds();
+                Element found = doc.GetElement(instanceCol.First());
+                if (found != null)
+                {
+                    List<Parameter> parameters = new List<Parameter>();
+                    foreach (Parameter p in found.Parameters)
+                    {
+                        if(p.StorageType == StorageType.Double || p.StorageType == StorageType.Integer)
+                        {
+                            parameters.Add(p);
+                        }
+                    }
+
+                    parameters.Sort((x, y) => x.Definition.Name.CompareTo(y.Definition.Name));
+
+                    paramListBox.ItemsSource = parameters;
+                    paramListBox.DisplayMemberPath = "Definition.Name";
+                    selectedSymbol = eType as FamilySymbol;
+                 }
+
+
+            }
+        }
+
+        private void cancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void cancelButton_MouseEnter(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void cancelButton_MouseLeave(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void randomizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            Parameter param = paramListBox.SelectedItem as Parameter;
+            if (param == null)
+                Autodesk.Revit.UI.TaskDialog.Show("Test", "Parameter is null");
+
+            FamilyInstanceFilter instFilter = new FamilyInstanceFilter(doc, selectedSymbol.Id);
+            ICollection<ElementId> instanceCol = new FilteredElementCollector(doc).OfCategoryId(selectedSymbol.Category.Id).WherePasses(instFilter).ToElementIds();
+
+            int minInt;
+            int maxInt;
+            if (!int.TryParse(minTextBox.Text, out minInt))
+                minInt = int.MinValue;
+            if (!int.TryParse(maxTextBox.Text, out maxInt))
+                maxInt = int.MinValue;
+
+            double minDouble;
+            double maxDouble;
+            if (!double.TryParse(minTextBox.Text, out minDouble))
+                minDouble = double.NaN;
+            if (!double.TryParse(maxTextBox.Text, out maxDouble))
+                maxDouble = double.NaN;
+
+            Random rand = new Random();
+
+            if (integerCheckBox.IsChecked.HasValue && integerCheckBox.IsChecked.Value)
+            {
+                if (minInt != int.MinValue && maxInt != int.MinValue)
+                {
+                    using (Transaction randomTrans = new Transaction(doc, "Randomize"))
+                    {
+                        randomTrans.Start();
+                        foreach (ElementId eid in instanceCol)
+                        {
+                            Element elem = doc.GetElement(eid);
+                            try
+                            {
+                                int r = rand.Next(minInt, maxInt);
+                                elem.get_Parameter(param.Definition).Set(r);
+                            }
+                            catch { }
+                        }
+                        randomTrans.Commit();
+                    }
+                }
+            }
+            else 
+            {
+                if (minDouble != double.NaN && maxDouble != double.NaN)
+                {
+                    using (Transaction randomTrans = new Transaction(doc, "Randomize"))
+                    {
+                        randomTrans.Start();
+
+                        foreach (ElementId eid in instanceCol)
+                        {
+                            Element elem = doc.GetElement(eid);
+                            try
+                            {
+                                double range = maxDouble - minDouble;
+                                double r = rand.NextDouble() * range + minDouble;
+                                if(param.Definition.ParameterType == ParameterType.Length)
+                                {
+                                    Units units = doc.GetUnits();
+                                    r = UnitUtils.ConvertToInternalUnits(r, param.DisplayUnitType);
+                                }
+                                elem.get_Parameter(param.Definition).Set(r);
+                            }
+                            catch (Exception ex) { Autodesk.Revit.UI.TaskDialog.Show("Error", ex.Message); }
+                        }
+
+                        randomTrans.Commit();
+                    }
+                }
+            }
+
+            Close();
+        }
+
+        private void randomizeButton_MouseEnter(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void randomizeButton_MouseLeave(object sender, MouseEventArgs e)
+        {
+
+        }
+    }
+}
